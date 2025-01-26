fad566a91550f5a10d1b5dbf72ef687e0fb16719
Chamikara Kodithuwakku <chamikaradimuth22@gmail.com>
2025-01-27 00:40:12 +0530

    initialize project setup

---
+++ src/main/java/lk/driveorbit/DriveOrbit_core/DriveOrbitCoreApplication.java
@@ -0,0 +1,13 @@
+package lk.driveorbit.DriveOrbit_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveOrbitCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveOrbitCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/driveorbit/DriveOrbit_core/DriveOrbitCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.driveorbit.DriveOrbit_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveOrbitCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

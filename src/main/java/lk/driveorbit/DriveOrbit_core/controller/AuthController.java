5e0587b1432e75efb6a08d1c5e2d189dd2c7c1b4
Chamikara Kodithuwakku <151379703+chaxmikara@users.noreply.github.com>
2025-02-28 10:43:30 +0530

    Revert "Feature: Implemented User Authentication via Backend Integration  (#2)" (#3)
    This reverts commit dc5d6430555c6d930161cfecf47d102ff9d0ace4.

--- src/main/java/lk/driveorbit/DriveOrbit_core/controller/AuthController.java
+++ src/main/java/lk/driveorbit/DriveOrbit_core/controller/AuthController.java
@@ -1,7 +1,6 @@
 package lk.driveorbit.DriveOrbit_core.controller;
 
 import lk.driveorbit.DriveOrbit_core.dto.AuthResponse;
-import lk.driveorbit.DriveOrbit_core.dto.UserIdRequest;
 import lk.driveorbit.DriveOrbit_core.model.Driver;
 import lk.driveorbit.DriveOrbit_core.security.JwtTokenProvider;
 import lk.driveorbit.DriveOrbit_core.service.UserService;
@@ -40,41 +39,25 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody Driver loginUser) {
-        try {
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginUser.getUserID(), loginUser.getPassword())
-            );
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginUser.getUserID(), loginUser.getPassword())
+        );
 
-            // if authentication is successful, set the authentication in the security context
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        // if authentication is successful, set the authentication in the security context
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-            // get the authenticated user
-            String currentUsername = authentication.getName();
-            Optional<Driver> user = userService.findByUserID(currentUsername);
+        // get the authenticated user
+        String currentUsername = authentication.getName();
+        Optional<Driver> user = userService.findByUserID(currentUsername);
 
-            if (user.isPresent()) {
-                // create a response with the token and role
-                String role = user.get().getRole();
-                String token = JwtTokenProvider.generateToken(currentUsername, role);
-
-                return ResponseEntity.ok(new AuthResponse(token, role));
-            }
-
-            return ResponseEntity.badRequest().body("Invalid company ID or password");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Invalid company ID or password");
-        }
-    }
-
-
-    @PostMapping("/get-email")
-    public ResponseEntity<String> getEmailByUserId(@RequestBody UserIdRequest userIdRequest) {
-        String userId = userIdRequest.getUserId();
-        Optional<Driver> user = userService.findByUserID(userId);
         if (user.isPresent()) {
-            return ResponseEntity.ok(user.get().getEmail());
-        } else {
-            return ResponseEntity.badRequest().body("User not found");
+            // create a response with the token and role
+            String role = user.get().getRole();
+            String token = JwtTokenProvider.generateToken(currentUsername, role);
+
+            return ResponseEntity.ok(new AuthResponse(token, role));
         }
+
+        return ResponseEntity.badRequest().build();
     }
 }

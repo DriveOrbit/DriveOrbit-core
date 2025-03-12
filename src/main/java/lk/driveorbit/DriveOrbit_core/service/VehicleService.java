9d611b8610656306228c63eed780cba012830663
Chamikara Kodithuwakku <chamikaradimuth22@gmail.com>
2025-03-12 20:38:55 +0530

    feat: return vehicle datas with web-socket

--- src/main/java/lk/driveorbit/DriveOrbit_core/service/VehicleService.java
+++ src/main/java/lk/driveorbit/DriveOrbit_core/service/VehicleService.java
@@ -1,8 +1,10 @@
 package lk.driveorbit.DriveOrbit_core.service;
 
 import lk.driveorbit.DriveOrbit_core.entity.Vehicle;
+import lk.driveorbit.DriveOrbit_core.model.VehicleMessage;
 import lk.driveorbit.DriveOrbit_core.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,9 +15,16 @@ public class VehicleService {
 
     @Autowired
     private VehicleRepository vehicleRepository;
+    
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     public Vehicle saveVehicle(Vehicle vehicle) {
-        return vehicleRepository.save(vehicle);
+        Vehicle savedVehicle = vehicleRepository.save(vehicle);
+        // Broadcast to all subscribers
+        VehicleMessage message = new VehicleMessage("UPDATE", savedVehicle);
+        messagingTemplate.convertAndSend("/topic/vehicles", message);
+        return savedVehicle;
     }
 
     public Optional<Vehicle> getVehicleById(Integer id) {
@@ -24,5 +33,13 @@ public class VehicleService {
 
     public List<Vehicle> getAllVehicles() {
         return vehicleRepository.findAll();
+    }
+    
+    public void deleteVehicle(Integer id) {
+        vehicleRepository.deleteById(id);
+        // Broadcast delete notification
+        VehicleMessage message = new VehicleMessage("DELETE", new Vehicle());
+        message.getVehicle().setVehicleId(id);
+        messagingTemplate.convertAndSend("/topic/vehicles", message);
     }
 }
